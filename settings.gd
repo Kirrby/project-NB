13cf3acecdfc042e14d5009b3a9d1a730d3b56a0
Kirrby <2933277055@qq.com>
2025-02-09 11:18:33 +0800

    创建了主菜单，和设置的大体框架

---
+++ settings.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_cancel_pressed() -> void:
+	get_tree().change_scene_to_file("res://loby.tscn")
+	pass # Replace with function body.
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		_on_cancel_pressed()
