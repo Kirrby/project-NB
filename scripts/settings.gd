6e0e4a0f0723843a502fab999d094b8522c333a3
Kirrby <2933277055@qq.com>
2025-02-12 10:33:19 +0800

    完善了setting中的窗口化逻辑

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -3,18 +3,18 @@ extends Control
 
 @onready var music_v: HSlider = $VBoxContainer/HBoxContainer/Music_v
 @onready var sound_v: HSlider = $VBoxContainer/HBoxContainer2/Sound_v
-@onready var win_mod: CheckButton = $VBoxContainer/HBoxContainer3/Win_mod
+@onready var win_opt: OptionButton = $VBoxContainer/HBoxContainer3/win_opt
 
-enum win_md { win, mini, max, full, sefu }
 
 func _ready():
 	config = load("res://Dialogue/config.tres")
-	
+	#将配置文件中的数值读取到UI中
 	music_v.value = config.music_volume
 	SoundManager.set_volume(2, music_v.value)
 	sound_v.value = config.sound_volume
-	win_mod.button_pressed = bool(config.window_mode)
-
+	win_opt.selected = win_opt.get_item_index(config.window_mode)
+	DisplayServer.window_set_mode(config.window_mode)
+	#连接UI信号
 	visibility_changed.connect(func ():
 		get_tree().paused = visible
 	)
@@ -22,15 +22,16 @@ func _ready():
 	music_v.value_changed.connect(func (v: float):
 		SoundManager.set_volume(2, v)
 	)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	
+	win_opt.item_selected.connect(func (index: int):
+		DisplayServer.window_set_mode(win_opt.get_item_id(index))
+	)
 
 
 func _on_cancel_pressed() -> void:
 	hide()
 	pass # Replace with function body.
+
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
@@ -43,16 +44,11 @@ func show_settings():
 	music_v.grab_focus()
 
 
-#func _on_confirm_pressed() -> void:
-	#config.music_volume  = music_v.value
-	#config.sound_volume = sound_v.value
-	#config.window_mode = int(win_mod.button_pressed)
-	#pass # Replace with function body.
-
 func _on_confirm_pressed() -> void:
+	#从UI读取配置
 	config.music_volume = music_v.value
 	config.sound_volume = sound_v.value
-	config.window_mode = int(win_mod.button_pressed)
+	config.window_mode = win_opt.get_item_id(win_opt.selected)
 	
 	# 保存配置资源
 	ResourceSaver.save(config, "res://Dialogue/config.tres")
